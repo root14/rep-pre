de72370318bd956ed64e3990360811861a6a5c4d
ilkay gavaz <ilkaygavaz2@gmail.com>
2025-04-25 11:27:35 +0300

    add spring web app

---
+++ rep-pre/src/main/java/com/root14/rep_pre/RepPreApplication.java
@@ -0,0 +1,13 @@
+package com.root14.rep_pre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepPreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepPreApplication.class, args);
+	}
+
+}
---
+++ rep-pre/src/test/java/com/root14/rep_pre/RepPreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root14.rep_pre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepPreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
